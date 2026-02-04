5590098a0a116f1b6d03944e8592b1aa2480abd4
anilvsvs25-code <anilvsvs25@gmail.com>
2026-02-04 17:24:44 +0530

    Add files via upload

---
+++ app.py.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Anilsharmavs@45",
+    database="employee_leave_db"
+)
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT leave_id, emp_id, start_date, end_date, reason, status FROM leaves")
+leaves = cursor.fetchall()
+
+print("\nLeave Requests:")
+for leave in leaves:
+    print(leave)
+
+leave_id = int(input("\nEnter Leave ID to update: "))
+new_status = input("Enter new status (APPROVED / REJECTED): ").upper()
+
+update_query = "UPDATE leaves SET status = %s WHERE leave_id = %s"
+cursor.execute(update_query, (new_status, leave_id))
+conn.commit()
+
+print("✅ Leave status updated successfully")
+
+conn.close()
